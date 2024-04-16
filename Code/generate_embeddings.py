c99174bed1978e9fb325350067ba301963809dc8
Artem <artem.kucheravy@gmail.com>
2024-04-16 22:54:19 +0300

    Write logic to generate embeddings in batches and store them using HDF5 file on a disk

--- Code/generate_embeddings.py
+++ Code/generate_embeddings.py
@@ -2,11 +2,21 @@
 from transformers import BertTokenizer, BertModel
 from utils import *
 import faiss
-import numpy as np
 from constants import path_res
 import os
 
 k = 5
+
+def get_pages_text_count(dataset_path : str, limit : int = -1):
+    processed = 0
+    if limit != -1:
+        return limit
+
+    for filename in os.listdir(dataset_path):
+        full_path = os.path.join(dataset_path, filename)
+        if os.path.isfile(full_path):
+            processed = processed + 1
+    return processed
 
 def get_pages_texts(dataset_path : str, max_limit : int = -1):
     texts = []
@@ -23,24 +33,65 @@ def get_pages_texts(dataset_path : str, max_limit : int = -1):
                     break
     return texts
 
+def build_index_from_hdf(file_path : str, dataset : str):
+    with h5py.File(file_path, 'r') as file:
+        if not dataset in file:
+            raise ValueError(f"Dataset {dataset} not in file")
+        index = faiss.IndexHNSWFlat(file[dataset].shape[1], 64)
+        index.add(file[dataset])
+        return index
 
+def get_embedding_from_hdf(file_path : str, dataset : str, index : int):
+    with h5py.File(file_path, 'r') as file:
+        if not dataset in file:
+            raise ValueError(f"Dataset {dataset} not in file")
+        return file[dataset][index]
+        
 if __name__ == '__main__':
     
+    index = None
     tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
     model = BertModel.from_pretrained('bert-base-multilingual-cased')
+    hdf5_file = path_res + f'\\embeddings.hdf5'
+    en_embeddings_name = "en_embeddings"
+    lv_embeddings_name = "lv_embeddings"
+    
+    #TODO
+    # Check if dataset {lv/en}_embeddings has as many entries as wikipedia texts are stored on a disk 
 
-    en_texts = get_pages_texts(path_res + "en_cirrussearch_title", 5)
-    en_embeddings = get_embeddings(en_texts, tokenizer, model).numpy()
-    dimensions = en_embeddings.shape[1]
+    try:
+        index = build_index_from_hdf(hdf5_file, en_embeddings_name)
+    except (FileNotFoundError, ValueError) as e:
+        # Generate file with english embeddings. Will take a lot of time...
+        en_texts = get_pages_texts(path_res + "en_cirrussearch_title", 30)
+        generate_embeddings(en_texts,  tokenizer, model, en_embeddings_name)
+        index = build_index_from_hdf(hdf5_file, en_embeddings_name)
+    except Exception as e:
+        print("Error:", repr(e))
+        exit()
 
-    index = faiss.IndexHNSWFlat(dimensions, 64)
-    index.add(en_embeddings.astype(np.float32))
+    try:
+        get_embedding_from_hdf(hdf5_file, lv_embeddings_name, 0)
+        text_count = get_pages_text_count(path_res + "lv_cirrussearch_title", 5)
+    except (ValueError, IndexError) as e:
+        lv_texts = get_pages_texts(path_res + "lv_cirrussearch_title", 5)
+        generate_embeddings(lv_texts,  tokenizer, model, lv_embeddings_name)
+        text_count = len(lv_texts)
+    
+    for i in range(text_count):
+        e = get_embedding_from_hdf(hdf5_file, lv_embeddings_name, i)
+        print(type(e))
 
-    lv_texts = get_pages_texts(path_res + "en_cirrussearch_title", 5)
-    lv_embeddings = get_embeddings(lv_texts, tokenizer, model).numpy()
-    for lv_embedding in lv_embeddings:
-        lv_embedding_2d = np.expand_dims(lv_embedding, axis=0)
-        D, I = index.search(lv_embedding_2d, k)
-        for i in I[0]:  
-            print(f"Index {i} text: {en_texts[i]}")
+
+
+    
+
+
+    # lv_texts = get_pages_texts(path_res + "en_cirrussearch_title", 5)
+    # lv_embeddings = get_embeddings(lv_texts, tokenizer, model)
+    # for lv_embedding in lv_embeddings:
+    #     lv_embedding_2d = np.expand_dims(lv_embedding, axis=0)
+    #     D, I = index.search(lv_embedding_2d, k)
+    #     for i in I[0]:  
+    #         print(f"Index {i} text: {en_texts[i]}")
         
--- Code/utils.py
+++ Code/utils.py
@@ -1,22 +1,47 @@
 import torch
 import requests
+import numpy as np
+import h5py
+from constants import path_res
 
 def cls_pooling(model_output):
-    # take embedding of first token of every sequence (CLS) token, which is a special classification token 
+    # take embedding of first token of every sequence (CLS) token, which is a special classification token
     return model_output.last_hidden_state[:, 0]
 
-def get_embeddings(text_list : list, tokenizer, model):
+def flush_embeddings(embeddings_list : list, group_name : str, cur_batch : int, total_vectors : int, emb_dimensions : int) -> None:
+    with h5py.File(path_res + f'\\embeddings.hdf5', 'a') as file:
+        if not group_name in file:
+            file.create_dataset(group_name, shape=(total_vectors, emb_dimensions))
+        for i in range(len(embeddings_list)):
+            embeddings_vector = embeddings_list[i]
+            file[group_name][cur_batch + i] = embeddings_vector
+
+def generate_embeddings(text_list, tokenizer, model, file_name : str, batch_size=10, log_file = None, ):
+    
     # calls tokenizer associated with provided model
     # which procceses input, so model is ready to work with it
     # padding = extend the sequences to the length of the longest sequence or max_length if provided
     # truncation = truncate longest sentences to max_length
     # max_length = maximum sequence length
     # https://huggingface.co/docs/transformers/pad_truncation padding to max model input length
-    encoded_input = tokenizer(text_list, padding='max_length', truncation=True, return_tensors="pt")
-    # torch.no_grad - we are NOT training. It does not store gradients during forward pass
-    with torch.no_grad():
-        model_output = model(**encoded_input)
-    return cls_pooling(model_output)
+
+    # Process texts in batches and flush each batch to the disk
+    for i in range(0, len(text_list), batch_size):
+        batch_texts = text_list[i:i + batch_size]
+        encoded_input = tokenizer(batch_texts, padding='max_length', truncation=True, return_tensors="pt")
+        
+        # torch.no_grad - we are NOT training. It does not store gradients during forward pass
+        with torch.no_grad():
+            model_output = model(**encoded_input)
+        embeddings = cls_pooling(model_output).numpy()
+
+        flush_embeddings(embeddings, file_name, i, len(text_list), embeddings.shape[1])
+        if log_file != None:
+            log_file.write(f"Flushed '{i + batch_size}' texts embeddings\n")
+        else:
+            print(f"Flushed '{i + batch_size}' units")
+
+    return embeddings.shape[1]
 
 def get_wiki_data(title : str, log_file = None):
 
