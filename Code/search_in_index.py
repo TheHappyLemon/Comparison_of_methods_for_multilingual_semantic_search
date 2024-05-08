47aac697ddcba30828e3b0a1d3eb90b42dc855ff
Artem <artem.kucheravy@gmail.com>
2024-05-08 21:38:28 +0300

    Remove test files + little fixes

--- Code/search_in_index.py
+++ Code/search_in_index.py
@@ -54,10 +54,6 @@ def search_in_dataset(path_to_csv : str, index : faiss.IndexHNSWFlat, query_data
 
         for i in range(len(file[query_dataset])):
             csv_row = {}
-
-            if i > 1 or (ignore_zeroes and i == zero_rows_indexes[0]):
-                break
-
             query_file  = file['mapping'][i][0].decode()
 
             # https://github.com/facebookresearch/faiss/issues/493
@@ -85,33 +81,32 @@ def search_in_dataset(path_to_csv : str, index : faiss.IndexHNSWFlat, query_data
             log_file.write(f"Flushed '{len(csv_rows)}' search results to csv file\n")
 
 if __name__ == '__main__':
-    hdf5_file = path_res + "embeddings_test.hdf5"
+    hdf5_file = path_res + "embeddings.hdf5"
     log_file = path_search + "search.log"
-    result_file = path_res + "search_result.csv"
     kNN = [1, 5, 10]
 
     datasets = get_dataset_names(hdf5_file)
 
     with open(log_file, 'a', encoding='utf-8') as log_file:
-        try:
-            for dataset in datasets:
-                index = build_index_from_hdf(hdf5_file, dataset)
-                log_file.write(f"Succefully created index for dataset '{dataset}'\n")
-                with h5py.File(hdf5_file, 'r') as file:
-                    query_dataset = datasets[dataset]
-                    if not query_dataset in file:
-                        raise ValueError(f"Query dataset '{query_dataset}' not in file '{hdf5_file}'")
-                    zero_rows_indexes = get_np_array_zero_rows(file[query_dataset])
-                    if len(zero_rows_indexes) != 0 and not ignore_zeroes:
-                        raise ValueError(f"Embedding in dataset '{query_dataset}' on index '{zero_rows_indexes[0]}' is not calculated!!!")
+        #try:
+        for dataset in datasets:
+            index = build_index_from_hdf(hdf5_file, dataset)
+            log_file.write(f"Succefully created index for dataset '{dataset}'\n")
+            with h5py.File(hdf5_file, 'r') as file:
+                query_dataset = datasets[dataset]
+                if not query_dataset in file:
+                    raise ValueError(f"Query dataset '{query_dataset}' not in file '{hdf5_file}'")
+                zero_rows_indexes = get_np_array_zero_rows(file[query_dataset])
+                if len(zero_rows_indexes) != 0 and not ignore_zeroes:
+                    raise ValueError(f"Embedding in dataset '{query_dataset}' on index '{zero_rows_indexes[0]}' is not calculated!!!")
 
-                    for k in kNN:
-                        path_to_csv = os.path.join(path_search, dataset.split('/')[1], dataset.split('/')[2], f"{k}NN.csv")
-                        start = datetime.now()
-                        log_file.write(f"Searching consequently for each element of '{query_dataset}'. k = '{k}'. Output to '{path_to_csv}'. Start time = '{start}'\n")
-                        search_in_dataset(path_to_csv, index, query_dataset, k, file, log_file) 
-                        log_file.write(f"Done. Execution time = '{datetime.now() - start}'\n")
-        except Exception as e:
-            log_file.write(f"Exception occured during searching: '{str(e)}'\n")
+                for k in kNN:
+                    path_to_csv = os.path.join(path_search, dataset.split('/')[1], dataset.split('/')[2], f"{k}NN.csv")
+                    start = datetime.now()
+                    log_file.write(f"Searching consequently for each element of '{query_dataset}'. k = '{k}'. Output to '{path_to_csv}'. Start time = '{start}'\n")
+                    search_in_dataset(path_to_csv, index, query_dataset, k, file, log_file) 
+                    log_file.write(f"Done. Execution time = '{datetime.now() - start}'\n")
+        #except Exception as e:
+            #log_file.write(f"Exception occured during searching: '{str(e)}'\n")
 
 
